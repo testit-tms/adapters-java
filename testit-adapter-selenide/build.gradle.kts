f307b4cb0bcb2405d92602343f0e04af3c32b691
Petr Komissarov <petr.komissarov@testit.software>
2024-05-21 17:53:07 +0300

    Fix cli props (#144)
    * Fix cli properties
    
    * Add rich test log

--- testit-adapter-selenide/build.gradle.kts
+++ testit-adapter-selenide/build.gradle.kts
@@ -35,20 +35,11 @@ tasks.test {
     useJUnitPlatform()
     testLogging {
         events = setOf(TestLogEvent.FAILED, TestLogEvent.SKIPPED, TestLogEvent.PASSED)
-        showCauses = false
-        showStackTraces = false
+        showCauses = true
+        showStackTraces = true
         showStandardStreams = true
     }
 
     systemProperty("junit.jupiter.extensions.autodetection.enabled", "true")
-    environment("TMS_URL", System.getProperty("tmsUrl"))
-    environment("TMS_PRIVATE_TOKEN", System.getProperty("tmsPrivateToken"))
-    environment("TMS_PROJECT_ID", System.getProperty("tmsProjectId"))
-    environment("TMS_CONFIGURATION_ID", System.getProperty("tmsConfigurationId"))
-    environment("TMS_TEST_RUN_ID", System.getProperty("tmsTestRunId"))
-    environment("TMS_TEST_RUN_NAME", System.getProperty("tmsTestRunName"))
-    environment("TMS_ADAPTER_MODE", System.getProperty("tmsAdapterMode"))
-    environment("TMS_CERT_VALIDATION", System.getProperty("tmsCertValidation"))
-    environment("TMS_TEST_IT", System.getProperty("tmsTestIt"))
-    environment("TMS_AUTOMATIC_CREATION_TEST_CASES", System.getProperty("tmsAutomaticCreationTestCases"))
+    systemProperties(systemProperties)
 }
