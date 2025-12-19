274c57b68b236281586eb1ecf838915ceb9e6976
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2025-12-19 15:41:08 +0300

    feat: TMS-661 add before control
    # Conflicts:
    #	testit-adapter-junit4/src/main/java/ru/testit/listener/BaseJunit4Listener.java

--- testit-adapter-junit4/src/main/java/ru/testit/listener/BaseJunit4Listener.java
+++ testit-adapter-junit4/src/main/java/ru/testit/listener/BaseJunit4Listener.java
@@ -1,10 +1,19 @@
 package ru.testit.listener;
 
+import static java.util.Objects.nonNull;
+
+import java.lang.reflect.Method;
+import java.util.UUID;
+import java.util.function.Consumer;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 import ru.testit.models.ClassContainer;
+import ru.testit.models.FixtureResult;
+import ru.testit.models.ItemStage;
 import ru.testit.models.ItemStatus;
 import ru.testit.models.MainContainer;
 import ru.testit.models.TestResult;
@@ -12,18 +21,25 @@ import ru.testit.services.Adapter;
 import ru.testit.services.AdapterManager;
 import ru.testit.services.ExecutableTest;
 
-import java.util.UUID;
-import java.util.function.Consumer;
-
-import static java.util.Objects.nonNull;
-
 @RunListener.ThreadSafe
 public class BaseJunit4Listener extends RunListener {
+
     private final AdapterManager adapterManager;
-    private final ThreadLocal<ExecutableTest> executableTest = ThreadLocal.withInitial(ExecutableTest::new);
-    private final ThreadLocal<String> launcherUUID = ThreadLocal.withInitial(() -> UUID.randomUUID().toString());
-    private final ThreadLocal<String> classUUID = ThreadLocal.withInitial(() -> UUID.randomUUID().toString());
-    private final ThreadLocal<Boolean> isFinished = ThreadLocal.withInitial(() -> false);
+    private final ThreadLocal<ExecutableTest> executableTest =
+        ThreadLocal.withInitial(ExecutableTest::new);
+    private final ThreadLocal<String> launcherUUID = ThreadLocal.withInitial(
+        () -> UUID.randomUUID().toString()
+    );
+    private final ThreadLocal<String> classUUID = ThreadLocal.withInitial(() ->
+        UUID.randomUUID().toString()
+    );
+    private final ThreadLocal<Boolean> isFinished = ThreadLocal.withInitial(
+        () -> false
+    );
+    // Fields to track fixture execution
+    private final ThreadLocal<String> beforeClassFixtureId =
+        new ThreadLocal<>();
+    private final ThreadLocal<String> beforeFixtureId = new ThreadLocal<>();
 
     public BaseJunit4Listener() {
         adapterManager = Adapter.getAdapterManager();
@@ -33,21 +49,103 @@ public class BaseJunit4Listener extends RunListener {
     public void testRunStarted(final Description description) {
         adapterManager.startTests();
 
-        final MainContainer mainContainer = new MainContainer()
-                .setUuid(launcherUUID.get());
+        final MainContainer mainContainer = new MainContainer().setUuid(
+            launcherUUID.get()
+        );
 
         adapterManager.startMainContainer(mainContainer);
 
-        final ClassContainer classContainer = new ClassContainer()
-                .setUuid(classUUID.get());
+        final ClassContainer classContainer = new ClassContainer().setUuid(
+            classUUID.get()
+        );
 
         adapterManager.startClassContainer(launcherUUID.get(), classContainer);
+
+        // Create fixtures for all @BeforeClass methods
+        createBeforeClassFixtures(description);
+
+        // Create fixtures for all @Before methods
+        createBeforeFixtures(description);
+    }
+
+    private void createBeforeClassFixtures(final Description description) {
+        // Get the test class
+        Class<?> testClass = description.getTestClass();
+        if (testClass == null) {
+            return;
+        }
+
+        // Look for @BeforeClass methods (create fixture for first one found)
+        Method[] methods = testClass.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(BeforeClass.class)) {
+                // Found a @BeforeClass method, create fixture if not already created
+                if (beforeClassFixtureId.get() == null) {
+                    String fixtureId = UUID.randomUUID().toString();
+                    beforeClassFixtureId.set(fixtureId);
+
+                    FixtureResult fixture = getFixtureResult(method);
+                    adapterManager.startPrepareFixtureAll(
+                        launcherUUID.get(),
+                        fixtureId,
+                        fixture
+                    );
+                }
+                break; // Only handle one for now, as in JUnit 5 implementation
+            }
+        }
+    }
+
+    private void createBeforeFixtures(final Description description) {
+        // Get the test class
+        Class<?> testClass = description.getTestClass();
+        if (testClass == null) {
+            return;
+        }
+
+        // Look for @Before methods (create fixture for first one found)
+        Method[] methods = testClass.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Before.class)) {
+                // Found a @Before method, create fixture if not already created
+                if (beforeFixtureId.get() == null) {
+                    String fixtureId = UUID.randomUUID().toString();
+                    beforeFixtureId.set(fixtureId);
+
+                    FixtureResult fixture = getFixtureResult(method);
+                    adapterManager.startPrepareFixtureEachTest(
+                        classUUID.get(),
+                        fixtureId,
+                        fixture
+                    );
+                }
+                break; // Only handle one for now, as in JUnit 5 implementation
+            }
+        }
     }
 
     @Override
     public void testRunFinished(final Result result) {
         if (Boolean.TRUE.equals(isFinished.get())) {
             return;
+        }
+
+        // Stop any running @BeforeClass fixture
+        if (beforeClassFixtureId.get() != null) {
+            adapterManager.updateFixture(beforeClassFixtureId.get(), f ->
+                f.setItemStatus(ItemStatus.PASSED)
+            );
+            adapterManager.stopFixture(beforeClassFixtureId.get());
+            beforeClassFixtureId.remove();
+        }
+
+        // Stop any running @Before fixture
+        if (beforeFixtureId.get() != null) {
+            adapterManager.updateFixture(beforeFixtureId.get(), f ->
+                f.setItemStatus(ItemStatus.PASSED)
+            );
+            adapterManager.stopFixture(beforeFixtureId.get());
+            beforeFixtureId.remove();
         }
 
         adapterManager.stopClassContainer(classUUID.get());
@@ -66,15 +164,28 @@ public class BaseJunit4Listener extends RunListener {
         final String uuid = test.getUuid();
         startTestCase(description, uuid);
 
-        adapterManager.updateClassContainer(classUUID.get(),
-                container -> container.getChildren().add(uuid));
+        adapterManager.updateClassContainer(classUUID.get(), container ->
+            container.getChildren().add(uuid)
+        );
     }
 
     @Override
     public void testFailure(final Failure failure) {
         ExecutableTest test = this.executableTest.get();
         test.setAfterStatus();
-        stopTestCase(test.getUuid(), failure.getException(), ItemStatus.FAILED);
+        stopTestCase(
+            test.getUuid(),
+            failure.getException(),
+            ItemStatus.FAILED
+        );
+
+        // Fail any running fixtures
+        if (beforeFixtureId.get() != null) {
+            adapterManager.updateFixture(beforeFixtureId.get(), f ->
+                f.setItemStatus(ItemStatus.FAILED)
+            );
+            adapterManager.stopFixture(beforeFixtureId.get());
+        }
     }
 
     @Override
@@ -99,8 +210,19 @@ public class BaseJunit4Listener extends RunListener {
             return;
         }
         test.setAfterStatus();
-        adapterManager.updateTestCase(test.getUuid(), setStatus(ItemStatus.PASSED, null));
+        adapterManager.updateTestCase(
+            test.getUuid(),
+            setStatus(ItemStatus.PASSED, null)
+        );
         adapterManager.stopTestCase(test.getUuid());
+
+        // Complete @Before fixture with success status
+        if (beforeFixtureId.get() != null) {
+            adapterManager.updateFixture(beforeFixtureId.get(), f ->
+                f.setItemStatus(ItemStatus.PASSED)
+            );
+            adapterManager.stopFixture(beforeFixtureId.get());
+        }
     }
 
     protected void startTestCase(Description method, final String uuid) {
@@ -109,28 +231,72 @@ public class BaseJunit4Listener extends RunListener {
         String testNode = fullName + "." + method.getMethodName();
 
         final TestResult result = new TestResult()
-                .setUuid(uuid)
-                .setLabels(Utils.extractLabels(method))
-                .setExternalId(Utils.extractExternalID(method))
-                .setWorkItemIds(Utils.extractWorkItemIds(method))
-                .setTitle(Utils.extractTitle(method))
-                .setName(Utils.extractDisplayName(method))
-                .setClassName(Utils.extractClassname(method, (index != -1) ? fullName.substring(index + 1) : fullName))
-                .setSpaceName(Utils.extractNamespace(method, (index != -1) ? fullName.substring(0, index) : null))
-                .setLinkItems(Utils.extractLinks(method))
-                .setDescription(Utils.extractDescription(method))
-                .setExternalKey(testNode);
+            .setUuid(uuid)
+            .setLabels(Utils.extractLabels(method))
+            .setExternalId(Utils.extractExternalID(method))
+            .setWorkItemIds(Utils.extractWorkItemIds(method))
+            .setTitle(Utils.extractTitle(method))
+            .setName(Utils.extractDisplayName(method))
+            .setClassName(
+                Utils.extractClassname(
+                    method,
+                    (index != -1) ? fullName.substring(index + 1) : fullName
+                )
+            )
+            .setSpaceName(
+                Utils.extractNamespace(
+                    method,
+                    (index != -1) ? fullName.substring(0, index) : null
+                )
+            )
+            .setLinkItems(Utils.extractLinks(method))
+            .setDescription(Utils.extractDescription(method))
+            .setExternalKey(testNode);
 
         adapterManager.scheduleTestCase(result);
         adapterManager.startTestCase(uuid);
     }
 
-    private void stopTestCase(final String uuid, final Throwable throwable, final ItemStatus status) {
-        adapterManager.updateTestCase(uuid, setStatus(status, throwable));
-        adapterManager.stopTestCase(uuid);
+    private FixtureResult getFixtureResult(final Method method) {
+        return new FixtureResult()
+            .setName(method.getName())
+            .setDescription("") // JUnit 4 doesn't have built-in description for setup methods
+            .setStart(System.currentTimeMillis())
+            .setItemStage(ItemStage.RUNNING);
     }
 
-    private Consumer<TestResult> setStatus(final ItemStatus status, final Throwable throwable) {
+    private void stopTestCase(
+        final String uuid,
+        final Throwable throwable,
+        final ItemStatus status
+    ) {
+        adapterManager.updateTestCase(uuid, setStatus(status, throwable));
+        adapterManager.stopTestCase(uuid);
+
+        // If test failed, also fail any running fixtures
+        if (status == ItemStatus.FAILED) {
+            if (beforeClassFixtureId.get() != null) {
+                adapterManager.updateFixture(beforeClassFixtureId.get(), f ->
+                    f.setItemStatus(ItemStatus.FAILED)
+                );
+                adapterManager.stopFixture(beforeClassFixtureId.get());
+                beforeClassFixtureId.remove();
+            }
+
+            if (beforeFixtureId.get() != null) {
+                adapterManager.updateFixture(beforeFixtureId.get(), f ->
+                    f.setItemStatus(ItemStatus.FAILED)
+                );
+                adapterManager.stopFixture(beforeFixtureId.get());
+                beforeFixtureId.remove();
+            }
+        }
+    }
+
+    private Consumer<TestResult> setStatus(
+        final ItemStatus status,
+        final Throwable throwable
+    ) {
         return result -> {
             result.setItemStatus(status);
             if (nonNull(throwable)) {
